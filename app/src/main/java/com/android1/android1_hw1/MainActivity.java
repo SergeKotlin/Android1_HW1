970f58f6d74a3405da5101738b10b3c71762fb08
Serge <jobsfolder@yandex.ru>
2021-03-21 17:54:44 +0300

    update HW1 with rec-s about switching layouts (tasks for Home Work 1 from Android1)

--- app/src/main/java/com/android1/android1_hw1/MainActivity.java
+++ app/src/main/java/com/android1/android1_hw1/MainActivity.java
@@ -2,6 +2,7 @@ package com.android1.android1_hw1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Switch aSwitch;
     private CheckBox checkBox1;
     private CheckBox checkBox2;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,16 +56,23 @@ public class MainActivity extends AppCompatActivity {
         checkBox1.setVisibility(View.VISIBLE);
         checkBox2.setVisibility(View.VISIBLE);
     }
+
+    // По совету сэмпая-ревьюера, переключение с макета на макет
+    public void switchLayout(View view) {
+        intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
 
 /* ✓ 1. Создать проект со следующими пользовательскими элементами: TextView, EditText, Button,
         Switch, CheckBox, ToggleButton.
  ✓ Для работы использовать LinearLayout.
  ✓ Использовать различные шрифты, цвета, размеры, прочие атрибуты.
-2. Создать ещё один макет (если не получается, то проект) с несколькими EditText,
+✓ 2. Создать ещё один макет (если не получается, то проект) с несколькими EditText,
 где использовать атрибут inputType: text, textPersonName, phone, number, textPassword,
 textEmailAddress и другие значения.
-3. Добавить в проект элемент CalendarView.
+✓ 3. Добавить в проект элемент CalendarView.
 4. * Разобраться, что такое параметр ems.
 * */
 // Serega, sure
